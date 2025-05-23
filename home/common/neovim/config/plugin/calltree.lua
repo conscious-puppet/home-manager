88649dda395021dfc7bfe162872cf80c52ea2129
Your Name <abhishek17513@gmail.com>
2025-05-24 00:36:21 +0530

    Refactor: Standardize plugin error notifications and remove unused plugins

--- home/common/neovim/config/plugin/calltree.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, calltree = pcall(require, "calltree")
-
-if not status_ok then
-  vim.notify("calltree not found!", vim.log.levels.ERROR)
-  return
-end
-
-calltree.setup()
--- home/common/neovim/config/plugin/kiwi-nvim.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, kiwi = pcall(require, "kiwi")
-
-if not status_ok then
-  vim.notify("kiwi not found!", vim.log.levels.ERROR)
-  return
-end
-
-kiwi.setup()
