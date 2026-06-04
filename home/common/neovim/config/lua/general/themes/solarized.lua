c86865e061993b601cb195ff916827787e0e8eeb
Abhishek Singh <abhishek17513@gmail.com>
2026-06-04 10:49:25 +0530

    fix nvim highlighting

--- home/common/neovim/config/lua/general/themes/solarized.lua
+++ home/common/neovim/config/lua/general/themes/solarized.lua
@@ -25,7 +25,7 @@ M.set = function()
   if theme_set then
     vim.api.nvim_set_hl(0, "StatusLine", M.statusline_colors.normal)
     vim.api.nvim_set_hl(0, "StatusLineNC", M.statusline_colors.nc)
-    vim.api.nvim_set_hl(0, "Visual", { bg = "#0a4454", default = false })
+    vim.api.nvim_set_hl(0, "Visual", { fg = "#002b36", bg = "#b58900", default = false })
     vim.api.nvim_set_hl(0, "SignColumn", { link = "LineNr", default = false })
     vim.api.nvim_set_hl(0, "WinSeparator", { fg = M.border_fg, bg = nil, default = false })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE", default = false })
